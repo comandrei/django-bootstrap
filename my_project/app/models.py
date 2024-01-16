d3945d6e79648eced1bc833d9258afa942bfdc24
Andrei Coman <comandrei@gmail.com>
2024-01-16 20:41:22 +0200

    Initial model setup

--- my_project/app/models.py
+++ my_project/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Produs(models.Model):
+    titlu = models.CharField(max_length=50)
+    pret = models.FloatField()
+    stoc = models.IntegerField(default=0)
+    descriere = models.CharField(max_length=1024)
+    imagine = models.FileField()
