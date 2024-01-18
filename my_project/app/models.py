9a8d848d753ea5142a8fd39d8763170d40a90c20
Andrei Coman <comandrei@gmail.com>
2024-01-18 19:52:12 +0200

    Add help_text

--- my_project/app/models.py
+++ my_project/app/models.py
@@ -5,5 +5,5 @@ class Produs(models.Model):
     titlu = models.CharField(max_length=50)
     pret = models.FloatField(db_index=True)
     stoc = models.IntegerField(default=0)
-    descriere = models.CharField(max_length=1024, null=True, blank=True)
+    descriere = models.CharField(max_length=1024, null=True, blank=True, help_text="Introduceti o descriere")
     imagine = models.FileField(null=True, blank=True)
