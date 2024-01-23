7c0c45252b10fc4fdd4095c09a79d0e0d023ac9d
Andrei Coman <comandrei@gmail.com>
2024-01-23 19:20:52 +0200

    Filter by price

--- my_project/app/views.py
+++ my_project/app/views.py
@@ -8,7 +8,10 @@ def salut(request):
     return HttpResponse("Salut")
 
 def lista_produse(request):
-    produse = Produs.objects.all()
+    if "pret_maxim" in request.GET:
+        produse = Produs.objects.filter(pret__lt=int(request.GET["pret_maxim"]))
+    else:
+        produse = Produs.objects.all()
     produse_formatat = [
         f"<li>{produs.titlu} - {produs.pret} - {produs.stoc}</li>"
         for produs in produse
