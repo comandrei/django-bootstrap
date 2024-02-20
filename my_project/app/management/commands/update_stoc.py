d826fc5169e3e341717d9467037b01e9ca4c8253
Andrei Coman <comandrei@gmail.com>
2024-02-20 19:33:43 +0200

    Management command

--- my_project/app/management/commands/update_stoc.py
+++ my_project/app/management/commands/update_stoc.py
@@ -4,9 +4,12 @@ from app.models import Produs
 class Command(BaseCommand):
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument('stoc', type=int)
+        parser.add_argument('--test', type=bool, default=False)
         return super().add_arguments(parser)
     
     def handle(self, *args, **options):
+        from pprint import pprint
+        pprint(options)
         print("In comanda")
         modified = Produs.objects.all().update(stoc=options['stoc'])
         print(f"Am actualizate {modified} produse")
